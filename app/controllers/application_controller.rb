359326274a57170f953fdd885d12cef985b2a4ee
private-ryoma <ryomann46497@gmail.com>
2023-07-16 16:30:46 +0900

    application_controllerにActiveRecordのHOSTを設定する処理追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    before_action :set_active_storage_host
+
+    private
+  
+    def set_active_storage_host
+      ActiveStorage::Current.host = request.base_url
+    end
 end
