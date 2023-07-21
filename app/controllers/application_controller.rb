8e85f041ca815258d87e2ef3be88dd842de21a9f
katou <katou@rsun.jp>
2023-07-21 12:55:20 +0900

    ActiveStorage::Current.url_optionsを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,6 @@ class ApplicationController < ActionController::Base
     def set_active_storage_host
       host = ENV["RAILS_ENV"] === "production" ? "https://railsec-75428b027bab.herokuapp.com" : "localhost:3000"
       ActiveStorage::Current.host = host
+      ActiveStorage::Current.url_options = { host: host }
     end
 end
