413bde8333ba3a71f46a5d935503200c82195ecb
katou <katou@rsun.jp>
2023-07-21 13:42:56 +0900

    application controllerのactivestorageへの設定変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,5 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-    before_action :set_active_storage_host
-
-    private
-  
-    def set_active_storage_host
-      host = ENV["RAILS_ENV"] === "production" ? "https://railsec-75428b027bab.herokuapp.com" : "localhost:3000"
-      ActiveStorage::Current.host = host
-      ActiveStorage::Current.url_options = { host: host }
-    end
+  include ActiveStorage::SetCurrent
 end
