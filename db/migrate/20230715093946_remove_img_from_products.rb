e6768cdd011edb46fa8c38d13bccbec4a82ab07a
private-ryoma <ryomann46497@gmail.com>
2023-07-22 09:39:21 +0900

    rubocopのエラー対応

--- db/migrate/20230715093946_remove_img_from_products.rb
+++ db/migrate/20230715093946_remove_img_from_products.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RemoveImgFromProducts < ActiveRecord::Migration[7.0]
   def change
     remove_column :products, :img, :string
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,59 +1,68 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  describe 'nameカラム' do
-    subject { Product.new(name: name, price: 100, description: "詳細説明", stock: 0) }
+  describe 'name' do
+    subject { described_class.new(name: name, price: 100, description: '詳細説明', stock: 0) }
 
-    context 'nameが空文字' do
+    context 'when nameが空文字' do
       let(:name) { '' }
+
       it { is_expected.to be_invalid }
     end
 
-    context 'nameが256文字以上' do
-      let(:name) { 'a'*256 }
+    context 'when nameが256文字以上' do
+      let(:name) { 'a' * 256 }
+
       it { is_expected.to be_invalid }
     end
   end
 
   describe 'price' do
-    subject { Product.new(name: "商品", price: price, description: "詳細説明", stock: 0) }
+    subject { described_class.new(name: '商品', price: price, description: '詳細説明', stock: 0) }
 
-    context 'priceが半角整数以外' do
-      let(:price) { "１００" }
+    context 'when priceが半角整数以外' do
+      let(:price) { '１００' }
+
       it { is_expected.to be_invalid }
     end
 
-    context 'priceが0未満' do
+    context 'when priceが0未満' do
       let(:price) { -100 }
+
       it { is_expected.to be_invalid }
     end
   end
 
-
   describe 'description' do
-    subject { Product.new(name: "商品", price: 100, description: description, stock: 0) }
+    subject { described_class.new(name: '商品', price: 100, description: description, stock: 0) }
 
-    context 'descriptionが空文字' do
-      let(:description) { "" }
+    context 'when descriptionが空文字' do
+      let(:description) { '' }
+
       it { is_expected.to be_invalid }
     end
 
-    context 'descriptionが256文字以上' do
-      let(:description) { "a" * 256 }
+    context 'when descriptionが256文字以上' do
+      let(:description) { 'a' * 256 }
+
       it { is_expected.to be_invalid }
     end
   end
 
   describe 'stock' do
-    subject { Product.new(name: "商品", price: 100, description: "詳細説明", stock: stock) }
+    subject { described_class.new(name: '商品', price: 100, description: '詳細説明', stock: stock) }
 
-    context 'stockが半角整数以外' do
-      let(:stock) { "１００" }
+    context 'when stockが半角整数以外' do
+      let(:stock) { '１００' }
+
       it { is_expected.to be_invalid }
     end
 
-    context 'stockがo未満' do
+    context 'when stockが0未満' do
       let(:stock) { -10 }
+
       it { is_expected.to be_invalid }
     end
   end
