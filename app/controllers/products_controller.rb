eba77d2c127de5df59647b089d61e5c21225d073
private-ryoma <ryomann46497@gmail.com>
2023-07-26 00:53:55 +0900

    詳細ページのコントローラー追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all.page(params[:page])
   end
+
+  def show
+    @product = Product.find(params[:id])
+    @latest_products = Product.all.where(['id != ?', @product.id]).order(:created_at).limit(4)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:id)
+  end
 end
